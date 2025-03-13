c979f2a546ec8df346eea126558a5301926cf7c6
dinar2491 <monsur2491@gmail.com>
2025-03-13 10:22:50 +0600

    Adding a Basic-operation

---
+++ T2ArithmeticOperation.java
@@ -0,0 +1,12 @@
+class arithmeticOperation
+{
+    public static void main(String[] args)
+    {
+        int x = 2, y = 5;
+
+        //Division
+        float result = (float) x / y;
+
+        System.out.print(result);
+    }
+}
