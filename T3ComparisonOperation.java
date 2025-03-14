e6ddf4f75f970eeb98b62c67e193f8d3d7aadadf
dinar2491 <monsur2491@gmail.com>
2025-03-14 10:34:07 +0600

    Adding a Basic-operation

---
+++ T3ComparisonOperation.java
@@ -0,0 +1,12 @@
+class comparisonOperation
+{
+    public static void main(String[] args)
+    {
+        int x = 2, y = 9;
+
+        //Checking the Lower
+        boolean result = x < y;
+
+        System.out.print(result);
+    }
+}
