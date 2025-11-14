9efe599e7b4a94cf6e740066eb2afe95363bd18d
dinar2491 <monsur2491@gmail.com>
2025-11-15 01:24:30 +0600

    Adding a Basic-operation

---
+++ 02Arithmetic-operation.java
@@ -0,0 +1,12 @@
+class arithmeticOperation
+{
+    public static void main(String[] args)
+    {
+        int a = 2, b = 5;
+
+        // Checking the 'Division'
+        float result = (float) a / b;
+
+        System.out.print(result);
+    }
+}
