4fb8a87339c1afed99a9fadb7344b7d05ce736a4
dinar2491 <monsur2491@gmail.com>
2025-12-21 11:56:42 +0600

    Adding a Basic-operation

---
+++ 03Comparison-operation.java
@@ -0,0 +1,12 @@
+class comparisonOperation
+{
+    public static void main(String[] args)
+    {
+        int a = 2, b = 9;
+
+        // Checking the 'Lower'
+        boolean result = a < b;
+
+        System.out.print(result);
+    }
+}
