1d39c21e1693c16f24eaf6333707bbf076aff2dd
dinar2491 <monsur2491@gmail.com>
2025-12-22 11:06:09 +0600

    Adding a Basic-operation

---
+++ 04Logical-operation.java
@@ -0,0 +1,12 @@
+class logicalOperation
+{
+    public static void main(String[] args)
+    {
+        int a = 2, b = 0, c = 3, d = 0;
+
+        // Checking the 'AND'
+        boolean result = a < b && c > d;
+
+        System.out.print(result);
+    }
+}
