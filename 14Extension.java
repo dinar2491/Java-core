d3bfb98d678195238014858cc33e32ba107d157e
dinar2491 <monsur2491@gmail.com>
2026-05-05 19:09:03 +0600

    Adding the Extension-operation

---
+++ 14Extension.java
@@ -0,0 +1,35 @@
+class A
+{
+    public A()
+    {
+        System.out.println("A as Default");
+    }
+
+    public A(int n)
+    {
+        System.out.println("A as Parameterized");
+    }
+}
+
+class B extends A
+{
+    public B()
+    {
+        System.out.print("B as Default");
+    }
+
+    public B(int n)
+    {
+        // super(n);
+        
+        System.out.print("B as Parameterized");
+    }
+}
+
+class Extension
+{
+    public static void main(String[] args)
+    {
+        B obj = new B(23);
+    }
+}
