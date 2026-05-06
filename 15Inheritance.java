51f9aee3981d627520bfe415867364469aff4845
dinar2491 <monsur2491@gmail.com>
2026-05-06 10:47:36 +0600

    Adding the Inheritance-operation

---
+++ 15Inheritance.java
@@ -0,0 +1,11 @@
+class Inheritance
+{
+    public static void main(String[] args)
+    {
+        Calculator obj = new Calculator();
+
+        float r = obj.output(2, 3);
+
+        System.out.print(r);
+    }
+}
